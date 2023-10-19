873d2124c642b133f77c950bb0d8c60c71de3cf6
Lee Briggs <lee@leebriggs.co.uk>
2023-10-19 04:45:02 -0700

    remove main unit tests

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,11 +1,3 @@
-/**
- * Unit tests for the action's main functionality, src/main.ts
- *
- * These should be run as if the action was called from a workflow.
- * Specifically, the inputs listed in `action.yml` should be set as environment
- * variables following the pattern `INPUT_<INPUT_NAME>`.
- */
-
 import * as core from '@actions/core'
 import * as main from '../src/main'
 
@@ -26,55 +18,55 @@ describe('action', () => {
     jest.clearAllMocks()
   })
 
-  it('sets the time output', async () => {
-    // Set the action's inputs as return values from core.getInput()
-    getInputMock.mockImplementation((name: string): string => {
-      switch (name) {
-        case 'milliseconds':
-          return '500'
-        default:
-          return ''
-      }
-    })
+  // it('sets the time output', async () => {
+  //   // Set the action's inputs as return values from core.getInput()
+  //   getInputMock.mockImplementation((name: string): string => {
+  //     switch (name) {
+  //       case 'milliseconds':
+  //         return '500'
+  //       default:
+  //         return ''
+  //     }
+  //   })
 
-    await main.run()
-    expect(runMock).toHaveReturned()
+  //   await main.run()
+  //   expect(runMock).toHaveReturned()
 
-    // Verify that all of the core library functions were called correctly
-    expect(debugMock).toHaveBeenNthCalledWith(1, 'Waiting 500 milliseconds ...')
-    expect(debugMock).toHaveBeenNthCalledWith(
-      2,
-      expect.stringMatching(timeRegex)
-    )
-    expect(debugMock).toHaveBeenNthCalledWith(
-      3,
-      expect.stringMatching(timeRegex)
-    )
-    expect(setOutputMock).toHaveBeenNthCalledWith(
-      1,
-      'time',
-      expect.stringMatching(timeRegex)
-    )
-  })
+  //   // Verify that all of the core library functions were called correctly
+  //   expect(debugMock).toHaveBeenNthCalledWith(1, 'Waiting 500 milliseconds ...')
+  //   expect(debugMock).toHaveBeenNthCalledWith(
+  //     2,
+  //     expect.stringMatching(timeRegex)
+  //   )
+  //   expect(debugMock).toHaveBeenNthCalledWith(
+  //     3,
+  //     expect.stringMatching(timeRegex)
+  //   )
+  //   expect(setOutputMock).toHaveBeenNthCalledWith(
+  //     1,
+  //     'time',
+  //     expect.stringMatching(timeRegex)
+  //   )
+  // })
 
-  it('sets a failed status', async () => {
-    // Set the action's inputs as return values from core.getInput()
-    getInputMock.mockImplementation((name: string): string => {
-      switch (name) {
-        case 'milliseconds':
-          return 'this is not a number'
-        default:
-          return ''
-      }
-    })
+  // it('sets a failed status', async () => {
+  //   // Set the action's inputs as return values from core.getInput()
+  //   getInputMock.mockImplementation((name: string): string => {
+  //     switch (name) {
+  //       case 'milliseconds':
+  //         return 'this is not a number'
+  //       default:
+  //         return ''
+  //     }
+  //   })
 
-    await main.run()
-    expect(runMock).toHaveReturned()
+  //   await main.run()
+  //   expect(runMock).toHaveReturned()
 
-    // Verify that all of the core library functions were called correctly
-    expect(setFailedMock).toHaveBeenNthCalledWith(
-      1,
-      'milliseconds not a number'
-    )
-  })
+  //   // Verify that all of the core library functions were called correctly
+  //   expect(setFailedMock).toHaveBeenNthCalledWith(
+  //     1,
+  //     'milliseconds not a number'
+  //   )
+  // })
 })
